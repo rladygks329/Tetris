5c31202da3ab947b188bf6ed11aaa39107bf076e
Kim Yo Han <rladygks329@gmail.com>
2023-08-16 00:09:19 +0900

    refactor: Tetromini

--- JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
+++ JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
@@ -6,26 +6,9 @@ public class OTetromino extends Tetromino {
     *  center point
     * */
     public OTetromino(int x, int y){
-        this.center = new Point(x, y);
-
-        Point[] points = new Point[3];
-        points[0] = new Point(x + 1, y);
-        points[1] = new Point(x, y -1);
-        points[2] = new Point(x + 1, y - 1);
-        this.others = points;
-    }
-    @Override
-    public Tetromino rotateLeft() {
-        return this;
-    }
-    @Override
-    public Tetromino rotateRight() {
-        return this;
-    }
-
-    @Override
-    public Tetromino down() {
-        Tetromino result = new OTetromino(center.x, center.y + 1);
-        return result;
+        points[0] = new Point();
+        points[1] = new Point();
+        points[2] = new Point();
+        points[3] = new Point();
     }
 }
