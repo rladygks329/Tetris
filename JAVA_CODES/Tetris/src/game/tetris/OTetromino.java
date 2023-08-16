39a2a7e8dc144b1413ad30c6f3ef84d260f42ac9
rladygks329 <rladygks329@gmail.com>
2023-08-16 13:07:42 +0900

    feat: Itetromino

--- JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
+++ JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
@@ -3,13 +3,10 @@ package game.tetris;
 public class OTetromino extends Tetromino {
 
   public OTetromino(int x, int y) {
-    // point point //
-    // center point //
     points[0] = new Point(x, y);
     points[1] = new Point(x, y - 1);
     points[2] = new Point(x + 1, y);
     points[3] = new Point(x + 1, y - 1);
-    center = points[0];
   }
 
   @Override
--- JAVA_CODES/Tetris/src/game/tetris/Point.java
+++ JAVA_CODES/Tetris/src/game/tetris/Point.java
@@ -1,27 +1,32 @@
 package game.tetris;
 
 public class Point {
-    public int x;
-    public int y;
+  public int x;
+  public int y;
 
-    public Point() {}
+  public Point() {}
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+  public Point(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public void setPoint(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj instanceof Point) {
+      Point opponent = ((Point) obj);
+      return x == opponent.x && y == opponent.y;
     }
+    return false;
+  }
 
-    public void setPoint(int x, int y){
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof Point){
-            Point opponent =  ((Point) obj);
-            return x == opponent.x && y == opponent.y;
-        }
-        return false;
-    }
+  @Override
+  public String toString() {
+    return "[POINT x: " + x + "y : " + y + "]";
+  }
 }
