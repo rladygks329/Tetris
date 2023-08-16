a1236849ca863b4439eb83cef7330eb3c33760b7
rladygks329 <rladygks329@gmail.com>
2023-08-16 10:15:47 +0900

    feat: down

--- JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
+++ JAVA_CODES/Tetris/src/game/tetris/OTetromino.java
@@ -1,14 +1,25 @@
 package game.tetris;
 
 public class OTetromino extends Tetromino {
-    /*
-    *  point point
-    *  center point
-    * */
-    public OTetromino(int x, int y){
-        points[0] = new Point();
-        points[1] = new Point();
-        points[2] = new Point();
-        points[3] = new Point();
-    }
+
+  public OTetromino(int x, int y) {
+    // point point //
+    // center point //
+    points[0] = new Point(x, y);
+    points[1] = new Point(x, y - 1);
+    points[2] = new Point(x + 1, y);
+    points[3] = new Point(x + 1, y - 1);
+    center = points[0];
+  }
+
+  @Override
+  public void rotateLeft() {
+    return;
+  }
+
+  @Override
+  public void rotateRight() {
+    return;
+  }
+
 }
