e06b51dc9425d10e852462597a52f82ffcc72cce
rladygks329 <rladygks329@gmail.com>
2023-08-16 13:07:42 +0900

    feat: Itetromino

--- JAVA_CODES/Tetris/src/game/tetris/Tetris.java
+++ JAVA_CODES/Tetris/src/game/tetris/Tetris.java
@@ -15,18 +15,82 @@ public class Tetris {
   public void down() {
     markOff(tetromino);
     tetromino.down();
+
     if (isValid(tetromino)) {
       markOn(tetromino);
+      score += 100;
     } else {
       tetromino.up();
       markOn(tetromino);
+      hanldeClear();
       tetromino = tmpFactory();
     }
+
   }
 
-  public void handleUserInput(int userInput) {}
+  public void left() {
+    markOff(tetromino);
+    tetromino.left();
+    if (!isValid(tetromino)) {
+      tetromino.right();
+    }
+    markOn(tetromino);
+  }
 
-  private void hanldeClear() {}
+  public void right() {
+    markOff(tetromino);
+    tetromino.right();
+    if (!isValid(tetromino)) {
+      tetromino.left();
+    }
+    markOn(tetromino);
+  }
+
+  public void rotateLeft() {
+    markOff(tetromino);
+    tetromino.rotateLeft();
+    if (!isValid(tetromino)) {
+      tetromino.rotateRight();
+    }
+    markOn(tetromino);
+  }
+
+  public void rotateRight() {
+    markOff(tetromino);
+    tetromino.rotateRight();
+    if (!isValid(tetromino)) {
+      tetromino.rotateLeft();
+    }
+    markOn(tetromino);
+  }
+
+  private void hanldeClear() {
+    int[][] map = board.map;
+    int line = 0;
+
+    for (int i = 0; i < Board.HEIGHT; i++) {
+      boolean isFull = true;
+
+      // 가로 라인 검사
+      for (int j = 0; j < Board.WIDTH; j++) {
+        if (map[i][j] == 0) {
+          isFull = false;
+          break;
+        }
+      }
+
+      if (!isFull) {
+        continue;
+      }
+
+      // 가로 라인 지우기
+      line += 1;
+      for (int j = 0; j < Board.WIDTH; j++) {
+        map[i][j] = 0;
+      }
+    }
+    score += line * 1000;
+  }
 
   private void reStrat() {}
 
@@ -39,20 +103,21 @@ public class Tetris {
     return true;
   }
 
+  // board에 tetromino를 표시한다.
   private void markOn(Tetromino t) {
     for (Point p : t.points) {
       board.mark(p, 1);
     }
-  }
+  } // end markOn()
 
+  // board에 tetromino를 제거한다.
   private void markOff(Tetromino t) {
     for (Point p : t.points) {
       board.mark(p, 0);
     }
-  }
+  } // end markOff()
 
-  private OTetromino tmpFactory() {
-    return new OTetromino(5, 1);
+  private Tetromino tmpFactory() {
+    return new ITetromino(5, 1);
   }
-
 }
--- JAVA_CODES/Tetris/src/game/tetris/TetrisView.java
+++ JAVA_CODES/Tetris/src/game/tetris/TetrisView.java
@@ -4,58 +4,99 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class TetrisView extends JPanel {
+  // constants
   private final String BACKGROUD_IMG_PATH = "res" + File.separator + "Tetriminos" + File.separator
       + "Board" + File.separator + "Board.png";
   private final String BLOCK_IMG_PATH =
       "res" + File.separator + "Tetriminos" + File.separator + "Single Blocks" + File.separator;
+  private final int KEY_CODE_LEFT = 37;
+  private final int KEY_CODE_RIGHT = 39;
+  private final int KEY_CODE_DOWN = 40;
+  private final int KEY_CODE_ROTATE_LEFT = 90;
+  private final int KEY_CODE_ROTATE_RIGHT = 88;
+  private final int BOX_SIZE = 30;
+
+  // member 변수
   private Tetris tetris;
   private Board board;
   private ActionListener gameLoop;
   private Timer timer;
-
-  int boxSize = 30;
-
   private Image bg;
   private Image[] blockImg;
 
   public TetrisView() {
-
     tetris = new Tetris();
     board = tetris.board;
 
     // init images
-    bg = getBlockImg(BACKGROUD_IMG_PATH, 12 * boxSize, 22 * boxSize);
+    bg = getBlockImg(BACKGROUD_IMG_PATH, 12 * BOX_SIZE, 22 * BOX_SIZE);
     blockImg = new Image[7];
-    blockImg[0] = getBlockImg(BLOCK_IMG_PATH + "Blue.png", boxSize, boxSize);
-    blockImg[1] = getBlockImg(BLOCK_IMG_PATH + "Green.png", boxSize, boxSize);
-    blockImg[2] = getBlockImg(BLOCK_IMG_PATH + "LightBlue.png", boxSize, boxSize);
-    blockImg[3] = getBlockImg(BLOCK_IMG_PATH + "Orange.png", boxSize, boxSize);
-    blockImg[4] = getBlockImg(BLOCK_IMG_PATH + "Purple.png", boxSize, boxSize);
-    blockImg[5] = getBlockImg(BLOCK_IMG_PATH + "Red.png", boxSize, boxSize);
-    blockImg[6] = getBlockImg(BLOCK_IMG_PATH + "Yellow.png", boxSize, boxSize);
+    blockImg[0] = getBlockImg(BLOCK_IMG_PATH + "Blue.png", BOX_SIZE, BOX_SIZE);
+    blockImg[1] = getBlockImg(BLOCK_IMG_PATH + "Green.png", BOX_SIZE, BOX_SIZE);
+    blockImg[2] = getBlockImg(BLOCK_IMG_PATH + "LightBlue.png", BOX_SIZE, BOX_SIZE);
+    blockImg[3] = getBlockImg(BLOCK_IMG_PATH + "Orange.png", BOX_SIZE, BOX_SIZE);
+    blockImg[4] = getBlockImg(BLOCK_IMG_PATH + "Purple.png", BOX_SIZE, BOX_SIZE);
+    blockImg[5] = getBlockImg(BLOCK_IMG_PATH + "Red.png", BOX_SIZE, BOX_SIZE);
+    blockImg[6] = getBlockImg(BLOCK_IMG_PATH + "Yellow.png", BOX_SIZE, BOX_SIZE);
 
+    // init game loop(thread)
     gameLoop = actionEvent -> {
       tetris.down();
       repaint();
     };
+
     timer = new Timer(500, gameLoop);
     timer.setInitialDelay(1000);
     timer.start();
+
+    // init keyListener
+    addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        handleUserInput(e.getKeyCode());
+      }
+    });
+
+  }
+
+  private void handleUserInput(int keyCode) {
+    switch (keyCode) {
+      case KEY_CODE_LEFT:
+        tetris.left();
+        break;
+      case KEY_CODE_RIGHT:
+        tetris.right();
+        break;
+      case KEY_CODE_DOWN:
+        tetris.down();
+        break;
+      case KEY_CODE_ROTATE_LEFT:
+        tetris.rotateLeft();
+        break;
+      case KEY_CODE_ROTATE_RIGHT:
+        tetris.rotateRight();
+        break;
+      default:
+        break;
+    }
+    repaint();
   }
 
   @Override
   public void paint(Graphics g) {
     super.paint(g);
-    g.drawImage(bg, 0, 0, 12 * boxSize, 22 * boxSize, this);
+    g.drawImage(bg, 0, 0, 12 * BOX_SIZE, 22 * BOX_SIZE, this);
     for (int i = 0; i < Board.HEIGHT; i++) {
       for (int j = 0; j < Board.WIDTH; j++) {
         int type = board.map[i][j];
-        g.drawImage(blockImg[type], (j + 1) * boxSize, (i + 1) * boxSize, this);
+        g.drawImage(blockImg[type], (j + 1) * BOX_SIZE, (i + 1) * BOX_SIZE, this);
       }
     }
   }// end paint()
