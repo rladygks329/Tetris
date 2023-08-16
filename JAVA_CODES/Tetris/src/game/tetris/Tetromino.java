82965d57ece40d9232c2b6576689507fc3a0b8b7
rladygks329 <rladygks329@gmail.com>
2023-08-16 15:09:45 +0900

    feat: Tetrominos

--- JAVA_CODES/Tetris/src/game/tetris/Tetromino.java
+++ JAVA_CODES/Tetris/src/game/tetris/Tetromino.java
@@ -1,36 +1,53 @@
 package game.tetris;
 
 abstract class Tetromino {
-  protected Point center;
   public final Point[] points = new Point[4];
-
-  protected Tetromino() {}
-
-  public abstract void rotateLeft();
-
-  public abstract void rotateRight();
+  protected Point center;
+  protected int length;
+  protected int index;
+  // based on - project/tetris_rotation_Nintendo.png
+  protected int[][][] map;
 
   public void down() {
     for (Point point : points) {
       point.setPoint(point.x, point.y + 1);
     }
-  }
+  } // end down()
 
   public void up() {
     for (Point point : points) {
       point.setPoint(point.x, point.y - 1);
     }
-  }
+  }// end up()
 
   public void left() {
     for (Point point : points) {
       point.setPoint(point.x - 1, point.y);
     }
-  }
+  }// end left()
 
   public void right() {
     for (Point point : points) {
       point.setPoint(point.x + 1, point.y);
     }
-  }
-}
+  }// end right()
+
+  public void rotateLeft() {
+    index = (length + index - 1) % length;
+    update();
+  }// end rotateLeft()
+
+  public void rotateRight() {
+    index = (index + 1) % length;
+    update();
+  }// end rotateRight()
+
+  private void update() {
+    int x = center.x;
+    int y = center.y;
+
+    for (int i = 0; i < points.length; i++) {
+      points[i].setPoint(x + map[index][i][0], y + map[index][i][1]);
+    }
+  }// end update()
+}// end Tetromino()
