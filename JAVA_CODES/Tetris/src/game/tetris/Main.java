72003216b820defa77c77c7082845f941f0aae24
rladygks329 <rladygks329@gmail.com>
2023-08-17 12:08:03 +0900

    feat: SignUpView

--- JAVA_CODES/Tetris/src/game/tetris/Main.java
+++ JAVA_CODES/Tetris/src/game/tetris/Main.java
@@ -34,7 +34,7 @@ public class Main extends JPanel {
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     System.out.println(this);
-    LoginView l = new LoginView(this);
+    SignInView l = new SignInView(this);
     l.setBounds(0, 0, 500, 500);
     l.setBorder(new LineBorder(Color.BLACK, 1));
     contentPanel = l;
@@ -50,25 +50,25 @@ public class Main extends JPanel {
   }
 
   public void navigate(JPanel panel) {
+    // 현재 panel 지우기
+    frame.getContentPane().remove(contentPanel);
+    frame.revalidate();
+    frame.repaint();
+
+    contentPanel = panel;
     if (panel instanceof TetrisView) {
-      // 현재 panel 지우기
-      frame.getContentPane().remove(contentPanel);
-      frame.revalidate();
-      frame.repaint();
-
-      // 테트리스 뷰 설정하기
-      TetrisView tv = (TetrisView) panel;
       frame.setBounds(0, 0, 700, 699);
-      tv.setBounds(0, 0, 12 * boxSize, 22 * boxSize);
-      tv.setBorder(new LineBorder(Color.BLACK, 1));
-      contentPanel = tv;
-
-      // panel 추가하고 다시 그리기
-      frame.getContentPane().add(tv);
-      tv.requestFocusInWindow();
-      frame.revalidate();
-      frame.repaint();
+    } else if (panel instanceof SignUpView) {
+      frame.setBounds(100, 100, 470, 400);
+    } else if (panel instanceof SignInView) {
+      frame.setBounds(100, 100, 470, 350);
     }
+
+    // panel 추가하고 다시 그리기
+    frame.getContentPane().add(panel);
+    panel.requestFocusInWindow();
+    frame.revalidate();
+    frame.repaint();
 
   }// end navigate
 }// end Main
--- JAVA_CODES/Tetris/src/game/tetris/SignInView.java
+++ JAVA_CODES/Tetris/src/game/tetris/SignInView.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -12,15 +14,16 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
-public class LoginView extends JPanel {
+public class SignInView extends JPanel {
+  private int KEY_CODE_ENTER = 10;
+
   private Main main;
+
   private JPasswordField passwordField;
   private JTextField textField;
   private JButton btnSubmit;
 
-  private int KEY_CODE_ENTER = 10;
-
-  public LoginView(Main main) {
+  public SignInView(Main main) {
     this.main = main;
     initialize();
   }
@@ -28,6 +31,8 @@ public class LoginView extends JPanel {
   private void initialize() {
     setBounds(100, 100, 450, 300);
     setLayout(null);
+
+    // <-- init Lables -->
     JLabel lblTitle = new JLabel("TETRIS");
     lblTitle.setFont(new Font("맑은 고딕", Font.BOLD, 30));
     lblTitle.setHorizontalAlignment(SwingConstants.CENTER);
@@ -40,41 +45,24 @@ public class LoginView extends JPanel {
     lblId.setBounds(12, 59, 135, 28);
     add(lblId);
 
-    btnSubmit = new JButton("Log In");
-    btnSubmit.addKeyListener(new KeyAdapter() {
-      @Override
-      public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KEY_CODE_ENTER) {
-          handleSubmit();
-        }
-      }
-    });
-    btnSubmit.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        handleSubmit();
-      }
-    });
-    btnSubmit.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
-    btnSubmit.setBounds(114, 238, 216, 41);
-    add(btnSubmit);
-
     JLabel lblPassword = new JLabel("Password");
     lblPassword.setFont(new Font("맑은 고딕", Font.BOLD, 20));
     lblPassword.setBounds(12, 134, 135, 28);
     add(lblPassword);
 
-    passwordField = new JPasswordField();
-    passwordField.addKeyListener(new KeyAdapter() {
+    JLabel lblSignUp = new JLabel("Sign Up");
+    lblSignUp.addMouseListener(new MouseAdapter() {
       @Override
-      public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KEY_CODE_ENTER) {
-          btnSubmit.requestFocus();
-        }
+      public void mouseClicked(MouseEvent e) {
+        navigateSignUp();
       }
     });
-    passwordField.setBounds(12, 169, 426, 34);
-    add(passwordField);
+    lblSignUp.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
+    lblSignUp.setBounds(12, 213, 64, 15);
+    add(lblSignUp);
 
+
+    // <-- init textField -- >
     textField = new JTextField();
     textField.addKeyListener(new KeyAdapter() {
       @Override
@@ -88,13 +76,40 @@ public class LoginView extends JPanel {
     add(textField);
     textField.setColumns(10);
 
-    JLabel lblSignUp = new JLabel("Sign Up");
-    lblSignUp.setFont(new Font("맑은 고딕", Font.PLAIN, 12));
-    lblSignUp.setBounds(12, 213, 64, 15);
-    add(lblSignUp);
+    passwordField = new JPasswordField();
+    passwordField.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KEY_CODE_ENTER) {
+          btnSubmit.requestFocus();
+        }
+      }
+    });
+    passwordField.setBounds(12, 169, 426, 34);
+    add(passwordField);
+
+    // <-- init buttton -->
+    btnSubmit = new JButton("Log In");
+    btnSubmit.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        handleSubmit();
+      }
+    });
+    btnSubmit.addKeyListener(new KeyAdapter() {
+      @Override
+      public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KEY_CODE_ENTER) {
+          handleSubmit();
+        }
+      }
+    });
+    btnSubmit.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+    btnSubmit.setBounds(114, 238, 216, 41);
+    add(btnSubmit);
   }
 
   private void handleSubmit() {
+    System.out.println("SignInView: handleSubmit");
     String id = textField.getText();
     String password = new String(passwordField.getPassword());
 
@@ -111,5 +126,10 @@ public class LoginView extends JPanel {
     }
     System.out.println("handleSubmit");
     main.navigate(new TetrisView());
+  }
+
+  private void navigateSignUp() {
+    SignUpView signUpView = new SignUpView(main);
+    main.navigate(signUpView);
   }
 }
