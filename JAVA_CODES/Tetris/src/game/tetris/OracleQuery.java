6b189002e3b4a18018598f56f4d92d1a6a056b4f
rladygks329 <rladygks329@gmail.com>
2023-08-17 18:06:18 +0900

    feat: DTO, query

---
+++ JAVA_CODES/Tetris/src/game/tetris/OracleQuery.java
@@ -0,0 +1,60 @@
+package game.tetris;
+
+public interface OracleQuery {
+  public static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+  public static final String USER = "scott";
+  public static final String PASSWORD = "tiger";
+
+  public static final String USER_TABLE_NAME = "TETRIS_USER";
+  public static final String USER_COL_USER_NO = "USER_NO";
+  public static final String USER_COL_USER_ID = "USER_ID";
+  public static final String USER_COL_PASSWORD = "PASSWORD";
+  public static final String USER_COL_NICKNAME = "NICKNAME";
+
+  public static final String SCORE_TABLE_NAME = "TETRIS_SCORE";
+  public static final String SCORE_COL_SCORE_NO = "SCORE_NO";
+  public static final String SCORE_COL_USER_NO = "USER_NO";
+  public static final String SCORE_COL_SCORE = "SCORE";
+  public static final String SCORE_COL_TIME = "TIME";
+
+  // 유저 추가
+  public static final String SQL_USER_INSERT =
+      "INSERT INTO " + USER_TABLE_NAME + " VALUES(TETRIS_USER_SEQ.NEXTVAL, ?, ?, ?)";
+
+  // 해당 유저의 비밀번호, 닉네임 업데이트
+  public static final String SQL_USER_UPDATE = "UPDATE " + USER_TABLE_NAME + " SET "
+      + USER_COL_PASSWORD + " = ?, " + USER_COL_NICKNAME + " = ? " + "WHERE USER_NO = ?";
+
+  // 비밀번호와 아이디를 가진 사람 찾기
+  public static final String SQL_SIGN_IN =
+      "SELECT " + USER_COL_USER_NO + ", " + USER_COL_PASSWORD + ", " + USER_COL_NICKNAME //
+          + " FROM " + USER_TABLE_NAME //
+          + " WHERE " + USER_COL_USER_ID + " = ? AND " + USER_COL_PASSWORD + " = ?";
+
+  // 같은 아이디를 가진 사람 수 세기
+  public static final String SQL_SELECT_USER_BY_ID =
+      "SELECT COUNT(*) FROM " + USER_TABLE_NAME + " WHERE " + USER_COL_USER_ID + " = ?";
+
+  // 같은 닉네임을 가진 사람 수 세기
+  public static final String SQL_SELECT_USER_BY_NICKNAME =
+      "SELECT COUNT(*) FROM " + USER_TABLE_NAME + " WHERE " + USER_COL_NICKNAME + " = ?";
+
+  // 유저 삭제
+  public static final String SQL_USER_DELETE = "DELETE " + USER_TABLE_NAME + " WHERE USER_NO = ?";
+
+  // 기록 추가 (USER_NO, SCORE)
+  public static final String SQL_SCORE_INSERT =
+      "INSERT INTO " + SCORE_TABLE_NAME + " VALUES(TETRIS_SCORE_SEQ.NEXTVAL, ?, ?)";
+
+  public static final String SQL_SCORE_SELECT_ALL = //
+      "SELECT " + SCORE_COL_SCORE + ", U." + USER_COL_NICKNAME + ", " + SCORE_COL_TIME + " "//
+          + "FROM " + SCORE_TABLE_NAME + " S JOIN " + USER_TABLE_NAME
+          + " U ON S.USER_NO = U.USER_NO "//
+          + "ORDER BY " + SCORE_COL_SCORE + " DESC";//
+
+  public static final String SQL_SCORE_SELECT_BY_USER_NO =
+      "SELECT " + SCORE_COL_USER_NO + ", " + SCORE_COL_SCORE + ", " + SCORE_COL_TIME//
+          + " FROM " + SCORE_TABLE_NAME //
+          + "  WHERE " + SCORE_COL_USER_NO + " = ?" //
+          + "  ORDER BY " + SCORE_COL_SCORE + " DESC";//
+}
