2793ed867ea7dedb6da6cbede669c1b1ca905369
rladygks329 <rladygks329@gmail.com>
2023-08-17 11:07:07 +0900

    feat: navigate multiple panel

--- JAVA_CODES/Tetris/src/game/tetris/Main.java
+++ JAVA_CODES/Tetris/src/game/tetris/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 public class Main extends JPanel {
+  private JPanel contentPanel;
   private JFrame frame;
   int boxSize = 30;
 
@@ -32,11 +33,12 @@ public class Main extends JPanel {
     frame.setBounds(100, 100, 470, 350);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-    LoginView l = new LoginView();
+    System.out.println(this);
+    LoginView l = new LoginView(this);
     l.setBounds(0, 0, 500, 500);
     l.setBorder(new LineBorder(Color.BLACK, 1));
+    contentPanel = l;
     frame.getContentPane().add(l);
-
 
     // frame.setBounds(100, 100, 720, 699);
     // TetrisView tv = new TetrisView();
@@ -47,4 +49,26 @@ public class Main extends JPanel {
     // frame.getContentPane().add(tv);
   }
 
-}
+  public void navigate(JPanel panel) {
+    if (panel instanceof TetrisView) {
+      // 현재 panel 지우기
+      frame.getContentPane().remove(contentPanel);
+      frame.revalidate();
+      frame.repaint();
+
+      // 테트리스 뷰 설정하기
+      TetrisView tv = (TetrisView) panel;
+      frame.setBounds(0, 0, 700, 699);
+      tv.setBounds(0, 0, 12 * boxSize, 22 * boxSize);
+      tv.setBorder(new LineBorder(Color.BLACK, 1));
+      contentPanel = tv;
+
+      // panel 추가하고 다시 그리기
+      frame.getContentPane().add(tv);
+      tv.requestFocusInWindow();
+      frame.revalidate();
+      frame.repaint();
+    }
+
+  }// end navigate
+}// end Main
