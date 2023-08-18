a6867b5070ecb1fce0e0e6ce7111c819393da2f8
rladygks329 <rladygks329@gmail.com>
2023-08-18 16:41:08 +0900

    refactor: dependency
    1. 공통 label home 라벨 로 변경
    2. I 이미지 가로로 변경
    3. TetrisKeyListener로 키 이벤트 이동

--- JAVA_CODES/Tetris/src/game/tetris/TetrisView.java
+++ JAVA_CODES/Tetris/src/game/tetris/TetrisView.java
@@ -6,12 +6,9 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.File;
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
@@ -26,14 +23,6 @@ public class TetrisView extends JPanel {
   private final String SHAPE_IMG_PATH =
       "res" + File.separator + "Tetrominos" + File.separator + "Shape Blocks" + File.separator;
   private final int BOX_SIZE = 30;
-  private final int SHAPE_SIZE = 120;
-
-  private final int KEY_CODE_LEFT = 37;
-  private final int KEY_CODE_RIGHT = 39;
-  private final int KEY_CODE_DOWN = 40;
-  private final int KEY_CODE_ROTATE_LEFT = 90;
-  private final int KEY_CODE_ROTATE_RIGHT = 88;
-  private final int KEY_CODE_HARDDROP = 32;
 
   // member 변수
   private Main main;
@@ -48,6 +37,7 @@ public class TetrisView extends JPanel {
   private Image[] shapeImg;
 
   public TetrisView(Main main) {
+    setBackground(Color.GRAY);
     this.main = main;
     user = main.user;
     initialize();
@@ -74,31 +64,21 @@ public class TetrisView extends JPanel {
     blockImg[7] = getResizeImg(BLOCK_IMG_PATH + "Yellow.png", BOX_SIZE, BOX_SIZE);
 
     shapeImg = new Image[8];
-    shapeImg[0] = getResizeImg(SHAPE_IMG_PATH + "I.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[1] = getResizeImg(SHAPE_IMG_PATH + "J.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[2] = getResizeImg(SHAPE_IMG_PATH + "S.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[3] = getResizeImg(SHAPE_IMG_PATH + "I.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[4] = getResizeImg(SHAPE_IMG_PATH + "L.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[5] = getResizeImg(SHAPE_IMG_PATH + "T.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[6] = getResizeImg(SHAPE_IMG_PATH + "Z.png", SHAPE_SIZE, SHAPE_SIZE);
-    shapeImg[7] = getResizeImg(SHAPE_IMG_PATH + "O.png", SHAPE_SIZE, SHAPE_SIZE);
+    shapeImg[0] = getResizeImg(SHAPE_IMG_PATH + "J.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[1] = getResizeImg(SHAPE_IMG_PATH + "J.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[2] = getResizeImg(SHAPE_IMG_PATH + "S.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[3] = getResizeImg(SHAPE_IMG_PATH + "I.png", BOX_SIZE * 4, BOX_SIZE * 1);
+    shapeImg[4] = getResizeImg(SHAPE_IMG_PATH + "L.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[5] = getResizeImg(SHAPE_IMG_PATH + "T.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[6] = getResizeImg(SHAPE_IMG_PATH + "Z.png", BOX_SIZE * 3, BOX_SIZE * 2);
+    shapeImg[7] = getResizeImg(SHAPE_IMG_PATH + "O.png", BOX_SIZE * 2, BOX_SIZE * 2);
 
     // init game loop(thread)
     gameLoop = actionEvent -> {
       tetris.down();
       repaint();
       if (tetris.state == -1) {
-        timer.stop();
-        dao.insert(user.getNo(), tetris.score);
-        int result = JOptionPane.showConfirmDialog(this,
-            "게임 오버\n Score " + tetris.score + "\n 다시 시작하시겠습니까?", "정보", JOptionPane.YES_NO_OPTION);
-        if (result == JOptionPane.YES_OPTION) {
-          tetris.restart();
-          repaint();
-          timer.restart();
-        } else {
-          main.navigate(new HomeView(main));
-        }
+        handleGameOver();
       }
     };
 
@@ -106,12 +86,7 @@ public class TetrisView extends JPanel {
     timer.setInitialDelay(1000);
     timer.start();
 
-    String HOME_IMG_PATH =
-        "res" + File.separator + "Tetrominos" + File.separator + "bg_btn_home.png";
-    Image img = Toolkit.getDefaultToolkit().getImage(HOME_IMG_PATH).getScaledInstance(50, 50,
-        java.awt.Image.SCALE_SMOOTH);
-
-    JLabel homeLabel = new JLabel(new ImageIcon(img));
+    JLabel homeLabel = new HomeLabel(50, 50);
     homeLabel.setBounds(624, 10, 50, 50);
     homeLabel.addMouseListener(new MouseAdapter() {
       @Override
@@ -123,43 +98,8 @@ public class TetrisView extends JPanel {
     add(homeLabel);
 
     // init keyListener
-    addKeyListener(new KeyAdapter() {
-      @Override
-      public void keyPressed(KeyEvent e) {
-        handleUserInput(e.getKeyCode());
-      }
-    });
+    addKeyListener(new TetrisKeyListener(tetris, this));
 
-  }
-
-  private void handleUserInput(int keyCode) {
-    if (tetris.state == -1) {
-      return;
-    }
-
-    switch (keyCode) {
-      case KEY_CODE_LEFT:
-        tetris.left();
-        break;
-      case KEY_CODE_RIGHT:
-        tetris.right();
-        break;
-      case KEY_CODE_DOWN:
-        tetris.down();
-        break;
-      case KEY_CODE_ROTATE_LEFT:
-        tetris.rotateLeft();
-        break;
-      case KEY_CODE_ROTATE_RIGHT:
-        tetris.rotateRight();
-        break;
-      case KEY_CODE_HARDDROP:
-        tetris.hardDrop();
-        break;
-      default:
-        break;
-    }
-    repaint();
   }
 
   @Override
@@ -172,16 +112,11 @@ public class TetrisView extends JPanel {
     g.drawImage(bg, 0, 0, width, height, this);
 
     // Score Board
-    g.setColor(Color.black);
-    g.fillRect(width + 10, 25, 200, 50);
     g.setColor(Color.white);
     g.setFont(new Font("맑은 고딕", Font.BOLD, 20));
     g.drawString("SCORE : " + Integer.toString(tetris.score), width + 20, 55);
 
     // Next Block
-    g.drawRect(width + 10, 75, 200, 175);
-    g.setColor(Color.black);
-    g.fillRect(width + 10, 75, 200, 175);
     g.drawImage(shapeImg[tetris.getNextBlock()], width + 30, 100, this);
     g.setColor(Color.white);
     g.drawString("NEXT BLOCK", width + 20, 80);
@@ -199,5 +134,19 @@ public class TetrisView extends JPanel {
   private Image getResizeImg(String imgPath, int width, int height) {
     return Toolkit.getDefaultToolkit().getImage(imgPath).getScaledInstance(width, height,
         java.awt.Image.SCALE_SMOOTH);
+  }// end getResizeImg()
+
+  private void handleGameOver() {
+    timer.stop();
+    dao.insert(user.getNo(), tetris.score);
+    int result = JOptionPane.showConfirmDialog(this,
+        "게임 오버\n Score " + tetris.score + "\n 다시 시작하시겠습니까?", "정보", JOptionPane.YES_NO_OPTION);
+    if (result == JOptionPane.YES_OPTION) {
+      tetris.restart();
+      repaint();
+      timer.restart();
+    } else {
+      main.navigate(new HomeView(main));
+    }
   }
-} // end getBlockImg()
+}
